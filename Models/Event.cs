01e133f919646944d78ec430a0f60203b6d967ae
MgangxelaXola-ST10439697 <164156933+MgangxelaXola-ST10439697@users.noreply.github.com>
2025-06-23 17:09:23 +0200

    Commit 3

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,22 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace EventEase2.Models
 {
+    [Table("Event")]
     public class Event
     {
         public int EventId { get; set; }
 
         // Foreign key to Venue
         public int VenueId { get; set; }
-        public Venue Venue { get; set; }
+        [ForeignKey("VenueId")]
+        public Venue? Venue { get; set; }
 
         // Foreign key to EventType
         public int EventTypeId { get; set; }
-        public EventType EventType { get; set; }
+        [ForeignKey("EventTypeId")]
+        public EventType? EventType { get; set; }
 
 
         // Event details
-        public string EventName { get; set; }
+        public string? EventName { get; set; }
         public DateTime EventDate { get; set; }
 
         public ICollection<Booking> Bookings { get; set; }
